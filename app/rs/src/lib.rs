f75d0e2ea451dab9cf6914405a3fdf8c271ff853
jch0029987-glitch <jch0029987@gmail.com>
2026-02-07 12:32:27 -0600

    Update lib.rs

--- app/rs/src/lib.rs
+++ app/rs/src/lib.rs
@@ -1,139 +1,76 @@
-use jni::objects::{JValueOwned, JObject, JString};
-use jni::sys::{jclass, jfloat, jint, jobject, jstring};
+use jni::objects::{JClass, JObject};
+use jni::sys::{jint, jobject};
 use jni::JNIEnv;
-use jni::{JavaVM, NativeMethod};
-use log::{error, info, LevelFilter};
-use std::ffi::c_void;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::thread;
-use android_logger::Config;
-use std::fs::File;
-use std::process::{Command, Stdio};
-
-mod input;
-mod renderer_bindings;
 
 use ndk::native_window::NativeWindow;
+use std::ptr::NonNull;
+use std::os::raw::c_void;
+use std::thread;
 
-macro_rules! jni_method {
-    ( $name: tt, $method:tt, $signature:expr ) => {{
-        jni::NativeMethod {
-            name: jni::strings::JNIString::from(stringify!($name)),
-            sig: jni::strings::JNIString::from($signature),
-            fn_ptr: $method as *mut c_void,
-        }
-    }};
-}
+mod input;
 
-static RENDERER_STARTED: AtomicBool = AtomicBool::new(false);
-
-#[no_mangle]
-pub extern "system" fn renderer_init(
-    mut env: JNIEnv,
-    _clz: jclass,
-    surface: jobject,
-    loader: jstring,
-    xdpi: jfloat,
-    ydpi: jfloat,
-    fps: jint,
-) {
-    let window = unsafe { NativeWindow::from_surface(env.get_native_interface(), surface) }
-        .expect("Failed to get NativeWindow from surface");
-    let width = window.width();
-    let height = window.height();
-
-    let window_ptr = window.ptr().as_ptr() as *mut c_void;
-
-    if RENDERER_STARTED.compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed).is_ok() {
-        info!("Initializing Twoyi Renderer: {}x{}", width, height);
-        input::start_input_system(width, height);
+/// Starts the OpenGL renderer on a separate thread
+pub fn start_renderer(window: Option<NativeWindow>, width: i32, height: i32, xdpi: f32, ydpi: f32, fps: i32) {
+    if let Some(window) = window {
+        // Wrap pointer in NonNull for thread safety
+        let window_ptr = NonNull::new(window.ptr().as_ptr() as *mut c_void)
+            .expect("Failed to get NonNull pointer from NativeWindow");
 
         thread::spawn(move || {
             unsafe {
-                renderer_bindings::startOpenGLRenderer(window_ptr, width, height, xdpi as i32, ydpi as i32, fps as i32);
+                // Call the FFI renderer
+                renderer_bindings::startOpenGLRenderer(
+                    window_ptr.as_ptr() as *mut c_void,
+                    width,
+                    height,
+                    xdpi as i32,
+                    ydpi as i32,
+                    fps as i32,
+                );
             }
         });
-
-        let loader_jstr = unsafe { JString::from_raw(loader) };
-        if let Ok(l_path) = env.get_string(&loader_jstr) {
-            let loader_path: String = l_path.into();
-            let log_path = "/data/data/io.twoyi/log.txt";
-            if let Ok(outputs) = File::create(log_path) {
-                let errors = outputs.try_clone().unwrap();
-                let _ = Command::new("./init")
-                    .current_dir("/data/data/io.twoyi/rootfs")
-                    .env("TYLOADER", loader_path)
-                    .stdout(Stdio::from(outputs))
-                    .stderr(Stdio::from(errors))
-                    .spawn();
-            }
-        }
     } else {
-        unsafe {
-            renderer_bindings::setNativeWindow(window_ptr);
-            renderer_bindings::resetSubWindow(window_ptr, 0, 0, width, height, width, height, 1.0, 0.0);
-        }
+        panic!("NativeWindow was None!");
     }
 }
 
+/// JNI function to reset or set a new window for the renderer
 #[no_mangle]
-pub extern "system" fn renderer_reset_window(mut env: JNIEnv, _clz: jclass, surface: jobject, _top: jint, _left: jint, _width: jint, _height: jint) {
-    let window = unsafe { NativeWindow::from_surface(env.get_native_interface(), surface) }
-        .expect("Failed to get NativeWindow from surface");
-    let window_ptr = window.ptr().as_ptr() as *mut c_void;
-    unsafe {
-        renderer_bindings::resetSubWindow(window_ptr, 0, 0, _width, _height, _width, _height, 1.0, 0.0);
+pub extern "system" fn renderer_reset_window(
+    env: JNIEnv,
+    _class: JClass,
+    surface: jobject,
+    _top: jint,
+    _left: jint,
+    _width: jint,
+    _height: jint,
+    xdpi: jint,
+    ydpi: jint,
+    fps: jint,
+) {
+    // Convert Java surface into NativeWindow
+    let window = unsafe { NativeWindow::from_surface(&env, JObject::from(surface)) };
+    
+    start_renderer(window, _width, _height, xdpi as f32, ydpi as f32, fps as i32);
+}
+
+/// JNI function to handle touch events forwarded from Java
+#[no_mangle]
+pub extern "system" fn handle_motion_event(env: JNIEnv, _class: JClass, motion_event: jobject) {
+    // Convert the Java MotionEvent into ndk::MotionEvent
+    if let Ok(ev) = ndk::event::MotionEvent::from_java(&env, motion_event) {
+        input::handle_touch(ev);
     }
 }
 
+/// Starts the input server (Unix socket) for multi-touch injection
 #[no_mangle]
-pub extern "system" fn renderer_remove_window(mut env: JNIEnv, _clz: jclass, surface: jobject) {
-    let window = unsafe { NativeWindow::from_surface(env.get_native_interface(), surface) }
-        .expect("Failed to get NativeWindow from surface");
-    let window_ptr = window.ptr().as_ptr() as *mut c_void;
-    unsafe {
-        renderer_bindings::removeSubWindow(window_ptr);
-    }
+pub extern "system" fn start_input(env: JNIEnv, _class: JClass, width: jint, height: jint) {
+    input::start_input_system(width, height);
 }
 
+/// Optionally send a key code event
 #[no_mangle]
-pub extern "system" fn handle_touch(mut env: JNIEnv, _clz: jclass, event: jobject) {
-    let obj = unsafe { JObject::from_raw(event) };
-
-    if let Ok(JValueOwned::Long(p)) = env.get_field(&obj, "mNativePtr", "J") {
-        let ev_ptr = p as *mut c_void;
-        if let Some(nonptr) = std::ptr::NonNull::new(ev_ptr) {
-            let ev = unsafe { ndk::event::MotionEvent::from_ptr(nonptr.cast()) };
-            input::handle_touch(ev);
-        }
-    }
-}
-
-#[no_mangle]
-pub extern "system" fn send_key_code(_env: JNIEnv, _clz: jclass, keycode: jint) {
-    input::send_key_code(keycode);
-}
-
-#[no_mangle]
-#[allow(non_snake_case)]
-unsafe fn JNI_OnLoad(jvm: JavaVM, _reserved: *mut c_void) -> jint {
-    android_logger::init_once(
-        Config::default()
-            .with_max_level(LevelFilter::Info)
-            .with_tag("CLIENT_EGL")
-    );
-
-    let mut env = jvm.get_env().unwrap();
-    let jni_methods = [
-        jni_method!(init, renderer_init, "(Landroid/view/Surface;Ljava/lang/String;FFI)V"),
-        jni_method!(resetWindow, renderer_reset_window, "(Landroid/view/Surface;IIII)V"),
-        jni_method!(removeWindow, renderer_remove_window, "(Landroid/view/Surface;)V"),
-        jni_method!(handleTouch, handle_touch, "(Landroid/view/MotionEvent;)V"),
-        jni_method!(sendKeycode, send_key_code, "(I)V"),
-    ];
-
-    let clazz = env.find_class("io/twoyi/Renderer").unwrap();
-    let _ = env.register_native_methods(&clazz, &jni_methods);
-
-    jni::sys::JNI_VERSION_1_6
+pub extern "system" fn send_key(env: JNIEnv, _class: JClass, code: jint) {
+    input::send_key_code(code);
 }
